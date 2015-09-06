236324ed8cf9b5d752937dc9fe4e1f3760b54aca
Spyros Desyllas <sdesyllas@hotmail.com>
2015-09-06 17:10:31 +0300

    header fragment

--- app/src/main/java/com/mpsp/spyros/loluniverse/MainActivity.java
+++ app/src/main/java/com/mpsp/spyros/loluniverse/MainActivity.java
@@ -1,11 +1,13 @@
 package com.mpsp.spyros.loluniverse;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements HeaderFragment.OnFragmentInteractionListener  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +35,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
